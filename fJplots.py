600f43472efc9d74052534199ecf65586b7e74b1
lposti <lorenzo.posti@gmail.com>
2015-03-09 10:44:46 +0100

    x-y labels: can be string or list of strings

--- fJplots.py
+++ fJplots.py
@@ -15,12 +15,13 @@ from fJmodel import FJmodel
 
 class PlotInterface(object):
 
-    def __init__(self, xlabel=None, ylabel=None,
+    def __init__(self, xlabel=None, ylabel=None, fontsize=15,
                  nrow=1, ncols=1, sharex=False, sharey=False, **fig_kw):
         """
         Constructor of the class
         :param xlabel: label for x axis (str, raw str)
         :param ylabel: label for y axis (str, raw str)
+        :param fontsize: fontsize for x-ylabels
         :param nrow: number of subplots in row (int)
         :param ncols: number of subplots in column (int)
         :param sharex: whether subplots must share x axis (bool)
@@ -29,12 +30,15 @@ class PlotInterface(object):
         :return: Initializes variables fig (plt.figure) and ax (list of plt.axes)
                  and nplots, idplot
         """
+        self.xlabel = None
         if xlabel is not None:
             self.xlabel = xlabel
 
+        self.ylabel = None
         if ylabel is not None:
             self.ylabel = ylabel
 
+        self.fontsize = fontsize
         self.nplots = nrow*ncols
         self.idplot = -1
         self.fig, self.ax = plt.subplots(nrow, ncols, sharex=sharex, sharey=sharey, **fig_kw)
@@ -61,6 +65,29 @@ class PlotInterface(object):
 
     def plotFigure(self, name=None):
 
+        if self.nplots == 1:
+            if self.xlabel is not None:
+                self.ax.set_xlabel(self.xlabel, fontsize=self.fontsize)
+            if self.ylabel is not None:
+                self.ax.set_ylabel(self.ylabel, fontsize=self.fontsize)
+        else:
+            if type(self.xlabel is list):
+                for i in range(self.nplots):
+                    if self.xlabel[i] is not None:
+                        self.ax[i].set_xlabel(self.xlabel[i], fontsize=self.fontsize)
+            else:
+                for i in range(self.nplots):
+                    if self.xlabel is not None:
+                        self.ax[i].set_xlabel(self.xlabel, fontsize=self.fontsize)
+
+            if type(self.ylabel is list):
+                for i in range(self.nplots):
+                    if self.ylabel[i] is not None:
+                        self.ax[i].set_ylabel(self.ylabel[i], fontsize=self.fontsize)
+            else:
+                for i in range(self.nplots):
+                    if self.ylabel is not None:
+                        self.ax[i].set_ylabel(self.ylabel, fontsize=self.fontsize)
         if self.idplot >= 0:
             if name is not None:
                 plt.savefig(name, bbox_inches='tight')
