51eb77f2bbaf44e8a3e947983c94bdccb358fecd
lposti <lorenzo.posti@gmail.com>
2015-06-11 17:22:01 +0200

    bug fix

--- fJmodel/kindata.py
+++ fJmodel/kindata.py
@@ -399,7 +399,7 @@ class KinData(object):
         for i in range(len(d)):
             flux.append(d[i][7])
 
-        flux = -2.5 * array(flux)
+        flux = -1. / 2.5 * array(flux)
 
         return flux
 
@@ -470,12 +470,13 @@ class KinData(object):
             Rmax = 20.  # f.ar[-1]
             x, y = f.project(inclination=inclination, nx=30, npsi=31, Rmax=Rmax)
 
+            model_density = power(10., f.dlos)
             # peaks of velocity moments, used for re-scaling the model
-            data_scale = npmax(flux[s])
-            model_scale = npmax(f.dlos)
+            data_scale = npmax(10. ** (flux[s])[xd])
+            model_scale = npmax(model_density)
 
-            plt.plot(x / npmax(x) * npmax(X_xd_pv), f.dlos[:, len(f.dlos) / 2] -
-                     model_scale + data_scale, 'b-')
+            plt.plot(x / npmax(x) * npmax(X_xd_pv), log10(model_density[:, len(model_density) / 2] /
+                     model_scale * data_scale), 'b-')
 
         else:
             plt.plot(X_pv, flux[s], 'b.')
